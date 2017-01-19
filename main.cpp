4a607c746d861ce8061fd17b2945f0a1dfc10674
Andrey Filipenkov <afi@iagentur.ch>
2017-01-19 11:07:49 +0100

    fix preferences plist name on macOS

--- main.cpp
+++ main.cpp
@@ -8,10 +8,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setApplicationVersion("0.1");
+    a.setApplicationVersion(QStringLiteral("0.1"));
     a.setApplicationName(QStringLiteral("HornAppDesktop"));
     a.setOrganizationName(QStringLiteral("kambala"));
-    a.setOrganizationDomain(QStringLiteral("com.kambala"));
 #ifdef Q_OS_WIN
     a.setFont(QFont("Segoe UI Symbol", 10));
 #endif
